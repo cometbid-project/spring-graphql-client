7a79a5d91e7ee0c1a3c66c5493abd49ba0c040d5
cometbid-project <20684020+cometbid-project@users.noreply.github.com>
2023-10-07 01:23:32 -0400

    changes to country record in graphql schema file

--- src/main/java/com/graphql/demo/country/Country.java
+++ src/main/java/com/graphql/demo/country/Country.java
@@ -9,13 +9,5 @@ import org.springframework.data.annotation.Id;
  * @author Gbenga
  *
  */
-public record Country (
-        @Id
-        Integer id,
-        String name,
-        String emoji,
-        String currency,
-        String code,
-        String capital
-) {
+public record Country(@Id Integer id, String name, String emoji, String currency, String code, String capital) {
 }
